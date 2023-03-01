f9ca7d6f0da8cc680dd88e7db2fd025231c87963
Ryan St. John <rstjohn@Ryans-MBP.home>
2023-03-01 14:37:28 -0500

    react project update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [song, setSongs] = useState([]);
+
+  let key = '';
+  let url = '';
+
+  // useEffect(() => {
+  //   fetch(url)
+  //   .then((response) => response.json())
+  //     .then((data) => setSongs(data));
+  // }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nowPlaying'><p>Now Playing</p></div>
+      <div className='songImg'><img src=''></img></div>
+      <div className='songTitle'><h2>Title</h2></div>
+      <div className='artistName'><h5>Name</h5></div>
+      <div className='controls'>
+        <button className='left'>{'<'}</button>
+        <button className='middle'>Play/Pause</button>
+        <button className='right'>{'>'}</button>
+      </div>
     </div>
   );
 }
